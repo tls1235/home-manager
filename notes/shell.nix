50cac627b7ff9eb2fec2a28caa396bdfa34c1339
tls1235 <yotam.mini3@gmail.com>
2026-08-03 15:47:59 +0300

    added a note for dev shells

---
+++ notes/shell.nix
@@ -0,0 +1,21 @@
+{
+  description = "should be changed to flake.nix and create a .envrc containing 'use flake' ";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+        ];
+        shellHook = "";
+      };
+    };
+}
